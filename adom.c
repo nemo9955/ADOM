e897178b845a702c0ed3bca94a7e078c64a8893b
nemo9955 <nemo9955@gmail.com>
2016-01-03 19:31:58 +0200

    removed cb related junk added commands to a linked list which can be shown with "!hst"

--- adom.c
+++ adom.c
@@ -16,30 +16,85 @@
 //http://www.linuxjournal.com/article/8603
 
 #define MAX_COMM 200 //maximum command length
-#define EXIT_COMM "!exit\n" //the comannd to initiate exit
+#define EXIT_COMM "!exit" //the comannd to initiate exit
 #define OO_COMM "\033[0;0H" // sets the cursor at 0 0 in the terminal
 #define SIM_COMM  KCYN " #! " KNRM // the "enter command" simbol thingy
 
-char in[MAX_COMM];
+struct list_el
+{
+    int index;
+    char comm[MAX_COMM];
+    struct list_el * next;
+};
+
+typedef struct list_el item;
+item * start, *end ;
+int indx;
+
+void addElem(char *p)
+{
+    item  * list = NULL;
+    list = (item *)malloc(sizeof(item));
+//    list->comm = (char *)malloc(sizeof(char)*strlen(p));
+    strcpy(list->comm , p);
+    list->index = ++indx;
+//    list->next  = NULL;
+
+    if(indx==1)
+        start=list ;
+    else
+        end->next = list;
+
+    end = list ;
+}
+
+void printAll()
+{
+    item *list = start ;
+
+    while(list)
+    {
+        printf("%s\n",list->comm);
+        list = list->next ;
+    }
+}
+
+int processComm(char * in)
+{
+    if(strcmp(in,"!hst")==0)
+    {
+        printf(KRED);
+        printf( "History : %d \n" KWHT,end->index);
+        printAll();
+        printf("\n");
+        return 1;
+    }
+
+    return 0;
+}
 
 int main()
 {
-    int i;
-    for(i=0; i<50; i++)//some sort of clear screen, might implement it as a command like "!clscr"
-        fprintf(stdout,"\n");
+    //clear the screen  -  updated cursor movcement from http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x361.html
+    fprintf(stdout,"\033[2J" OO_COMM);
 
-    fprintf(stdout,OO_COMM);//sets the cursor at terminal char coords 0 0
-
+    char in[MAX_COMM];
     while(strcmp(in,EXIT_COMM))
     {
         fprintf(stdout,SIM_COMM  );
         fgets(in,MAX_COMM,stdin);
+        in[strlen(in)-1] = '\0';
+        addElem(in);
+        switch(processComm(in))
+        {
+        }
     }
 
     /* TODO
     *
+    *   + add commands to a list , !hst to show
     *   add history
-    *   add external commands
+    *   add external commands support
     *
     *
     *
@@ -48,11 +103,3 @@ int main()
 
     return 0;
 }
-//    printf("%sred", KRED);
-//    printf("%sgreen", KGRN);
-//    printf("%syellow", KYEL);
-//    printf("%sblue", KBLU);
-//    printf("%smagenta", KMAG);
-//    printf("%scyan", KCYN);
-//    printf("%swhite", KWHT);
-//    printf("%snormal", KNRM);
